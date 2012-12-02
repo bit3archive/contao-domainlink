12fae2fcb371b0643ffe6f9138c8d7af100bd45d
Tristan Lins <tristan.lins@bit3.de>
2012-12-02 20:24:47 +0100

    Add autoload.php.

---
+++ system/modules/DomainLink/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package DomainLink
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'DomainLink' => 'system/modules/DomainLink/DomainLink.php',
+));
